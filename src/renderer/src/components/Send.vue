33db78cd17dd0f60e1cfae2bca82f9bcde1e63f6
ItzJerry <85010676+ItzJerry317@users.noreply.github.com>
2026-02-25 10:06:51 +0800

    fix(socket,ui): 修复在信任设备列表点击断开连接后不能正常获取取件码的问题

--- src/renderer/src/components/Send.vue
+++ src/renderer/src/components/Send.vue
@@ -530,7 +530,7 @@ onMounted(() => {
                     连接
                   </v-btn>
                   <v-btn size="small" color="error" variant="tonal" class="mr-2"
-                    :disabled="!device.isOnline || !isP2PReady || device.id !== connectedPeerId" @click="leaveRoom()"
+                    :disabled="!device.isOnline || !isP2PReady || device.id !== connectedPeerId" @click="handleDisconnect()"
                     v-if="connectedPeerId === device.id">
                     断开
                   </v-btn>
