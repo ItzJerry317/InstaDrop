ec7101f6345bfbb626bd10d33b1f2b54f7c20ed2
ItzJerry <85010676+ItzJerry317@users.noreply.github.com>
2026-02-27 17:46:38 +0800

    chore: 修改版本号至v1.1.1

--- src/renderer/src/components/Settings.vue
+++ src/renderer/src/components/Settings.vue
@@ -22,7 +22,7 @@ const toggleAutoStart = async () => {
 }
 
 // === 检查更新状态 ===
-const currentVersion = ref('v1.1.0') // 你的当前版本号
+const currentVersion = ref('v1.1.1') // 你的当前版本号
 const isCheckingUpdate = ref(false)
 const hasNewVersion = ref(false)
 const showUpdateDialog = ref(false)
